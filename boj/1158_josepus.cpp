840049c51396e0162eff30d06103a7ea1ded11a6
dohkim <dh54kim54@gmail.com>
2020-05-14 23:05:44 +0900

    josepus done

--- boj/1158_josepus.cpp
+++ boj/1158_josepus.cpp
@@ -6,7 +6,7 @@
 /*   By: dohkim <dohkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 16:45:20 by dohkim            #+#    #+#             */
-/*   Updated: 2020/04/27 20:42:22 by dohkim           ###   ########.fr       */
+/*   Updated: 2020/05/14 23:05:03 by dohkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,26 @@ int main()
 	int N,K;
 	list<int> L;
 	cin >> N >> K;
-	
-	return (0);
+	for(int i=1; i<=N; i++)
+	{
+		L.push_back(i);
+	}
+	list<int>::iterator t = L.begin();
+	cout << "<";
+	while (!L.empty())
+	{
+		for (int i = 1; i < K; i++)
+		{
+			++t;
+			if (t == L.end())
+				t = L.begin();
+		}
+		cout << *t;
+		t = L.erase(t);
+		if (t == L.end())
+			t = L.begin();
+		if (!L.empty())
+			cout << ", ";	
+	}
+	cout << ">";
 }
