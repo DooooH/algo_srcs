d334d5631a32c8158fd905675d9c9413d3698819
Dohyung Kim <dohkim@c2r1s3.42seoul.kr>
2020-07-04 09:41:38 +0900

    nav finish

--- tree/navigate.cpp
+++ tree/navigate.cpp
@@ -18,51 +18,30 @@ struct TreeNode{
     TreeNode(int x, int y, int z, TreeNode *left, TreeNode *right) : index(x), location(y,z), left(left), right(right) {}
 };
 
-// bool comp(pair<int, int> a, pair<int, int> b)
-// {
-// 	return a.first < b.first;
-// }
-
 void tree_insert(TreeNode *parent, TreeNode *child)
 {
 	if (child->location.first < parent->location.first)
 	{
-		if (parent->left != nullptr)
-		{
+		if (parent->left == nullptr)
 			parent->left = child;
-			return ;
-		}
 		else
-		{
-			parent = parent->left;
-			tree_insert(parent, child);
-		}
+			tree_insert(parent->left, child);
 	}
 	else
 	{
-		if (parent->right != nullptr)
-		{
+		if (parent->right == nullptr)
 			parent->right = child;
-			return ;
-		}
 		else
-		{
-			parent = parent->right;
-			tree_insert(parent, child);
-		}
+			tree_insert(parent->right, child);
 	}
 }
 
-TreeNode *make_node(int x, int y, int z)
+TreeNode *make_node(TreeNode **ptr, int x, int y, int z)
 {
-	TreeNode *ptr;
-	
-	if ((ptr = (TreeNode *)malloc(sizeof(TreeNode))) == 0)
+	if ((*ptr = (TreeNode *)malloc(sizeof(**ptr))) == 0)
 		return nullptr;
-	ptr->index = x;
-	ptr->location.first = y;
-	ptr->location.second = z;
-	return ptr;
+	**ptr = TreeNode(x, y, z);
+	return *ptr;
 }
 
 void preorder(TreeNode *node, vector<int> &answer)
@@ -79,8 +58,8 @@ void postorder(TreeNode *node, vector<int> &answer)
 {
 	if (node != nullptr)
 	{
-		preorder(node->left, answer);
-		preorder(node->right, answer);
+		postorder(node->left, answer);
+		postorder(node->right, answer);
 		answer.push_back(node->index);
 	}
 }
@@ -108,41 +87,24 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
 		m.insert(m_temp);
 	}
 
-
-	// for (auto t = m.begin(); t != m.end(); t++)
-	// {
-	// 	cout << (*t).second << " ";
-	// }
 	map<pair<int, int>, int, comparator>::iterator it;
-	// root->index = (*it).second;
-	// root->location.first = (*it).first.first;
-	// root->location.second = (*it).first.second;
-	// cout << root->index << " " << root->location.first << " " << root->location.second << "\n";
 	for (it = m.begin(); it != m.end(); it++)
 	{
+		TreeNode *node;
+
 		if (it == m.begin())
-			root = make_node((*it).second, (*it).first.first, (*it).first.second);
+			root = make_node(&node, (*it).second, (*it).first.first, (*it).first.second);
 		else
-			tree_insert(root, make_node((*it).second, (*it).first.first, (*it).first.second));
+			tree_insert(root, make_node(&node, (*it).second, (*it).first.first, (*it).first.second));
 	}
 	
 	vector<int> temp_vector;
 	preorder(root, temp_vector);
 	answer.push_back(temp_vector);
-	for (auto t = temp_vector.begin(); t != temp_vector.end(); t++)
-	{
-		cout << *t << " ";
-	}
-	cout << "\n";
 	temp_vector.clear();
+
 	postorder(root, temp_vector);
 	answer.push_back(temp_vector);
-
-	for (auto t = temp_vector.begin(); t != temp_vector.end(); t++)
-	{
-		cout << *t << " ";
-	}
-	cout << "\n";
     return answer;
 }
 
@@ -152,16 +114,3 @@ int main()
 	solution(nodeinfo);
 	return 0;
 }
-
-
-// vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-//     vector<vector<int>> answer;
-
-// 	// cout << (*max(answer.begin(), answer.end(), cmp))[1] << "\n";
-  
-// 	for (auto iter = m.begin(); iter != m.end(); iter++)
-// 	{
-// 		cout << (*iter).second << " / " << (*iter).first.first << " " << (*iter).first.second << "\n";
-// 	}
-//     return answer;
-// }
